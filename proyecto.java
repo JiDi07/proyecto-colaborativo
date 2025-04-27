ebca933f5cd49268e3a672057311ab6a70703bab
fernanda <marygfhj@gmail.com>
2025-04-27 15:03:30 +0200

    añado una linea en el proyecto java

--- proyecto.java
+++ proyecto.java
@@ -1,5 +1,6 @@
 public class proyecto{
 public static void main(String [] args){
   System.out.println("Hello, Jesus, and Holy Spirit");
+  System.out.println("esto es un cambio desde mi rama");
 }
 }
