01ffd7e681f0827efdf26c49efaea98aa9f74b2f
fernanda <marygfhj@gmail.com>
2025-04-27 15:58:02 +0200

    añado lineas en le proyecto java

--- proyecto.java
+++ proyecto.java
@@ -1,5 +1,39 @@
-public class proyecto{
-public static void main(String [] args){
-  System.out.println("Hello, Jesus, and Holy Spirit");
-}
+import java.util.Scanner;
+
+public class proyecto {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Ingresa un número del 1 al 7: ");
+    int numero = scanner.nextInt();
+    String dia;
+
+    switch (numero) {
+      case 1:
+        dia = "Lunes";
+        break;
+      case 2:
+        dia = "Martes";
+        break;
+      case 3:
+        dia = "Miércoles";
+        break;
+      case 4:
+        dia = "Jueves";
+        break;
+      case 5:
+        dia = "Viernes";
+        break;
+      case 6:
+        dia = "Sábado";
+        break;
+      case 7:
+        dia = "Domingo";
+        break;
+      default:
+        dia = "Número inválido. Debe ser del 1 al 7.";
+    }
+
+    System.out.println("El día correspondiente es: " + dia);
+  }
 }
